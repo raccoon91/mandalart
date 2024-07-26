8be17f16da7c8e3a412d5cef6ea6ffa12bb3d5f1
raccoon91 <dev.beomseok@gmail.com>
2024-07-26 23:42:30 +0900

    feat: add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:mandalart/app.dart';
 import 'package:mandalart/db/isar_db.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   await IsarDB().init();
+
+  FlutterNativeSplash.remove();
 
   runApp(const App());
 }
