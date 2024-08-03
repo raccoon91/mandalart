0f476413ee6c0c7d339c228d729ec4adc8066e20
raccoon91 <dev.beomseok@gmail.com>
2024-08-03 19:40:43 +0900

    feat: add admob init

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:mandalart/app.dart';
 import 'package:mandalart/db/isar_db.dart';
 
@@ -7,6 +8,8 @@ void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
 
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await MobileAds.instance.initialize();
 
   await IsarDB().init();
 
