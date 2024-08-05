7a77911b1d8beb26e255fe622912a4e9c616d437
raccoon91 <dev.beomseok@gmail.com>
2024-08-05 22:34:08 +0900

    feat: add bottom navigation animation

--- lib/widget/layout/bottom_navigation_layout.dart
+++ lib/widget/layout/bottom_navigation_layout.dart
@@ -31,43 +31,47 @@ class BottomNavigationLayout extends StatelessWidget {
       title: title,
       body: child,
       showFloatingAction: screenName == 'home',
-      bottomNavigationBar: Container(
-        padding: branchIndex == 0
-            ? EdgeInsets.only(right: 20.w, bottom: 20.h, left: 20.w)
-            : const EdgeInsets.all(0),
+      bottomNavigationBar: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
         height: branchIndex == 0 ? 80.h : 60.h,
-        child: ClipRRect(
-          borderRadius: BorderRadius.all(
-            branchIndex == 0
-                ? Radius.circular(50.r)
-                : const Radius.circular(0),
-          ),
-          child: BottomNavigationBar(
-            currentIndex: child.currentIndex,
-            selectedFontSize: 0,
-            unselectedFontSize: 0,
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            backgroundColor: ColorClass.under,
-            selectedItemColor: ColorClass.blue,
-            unselectedItemColor: ColorClass.border,
-            onTap: bottomItemTapped,
-            items: const [
-              BottomNavigationBarItem(
-                label: 'Home',
-                icon: SizedBox(
-                  child: Icon(Icons.home_filled),
+        child: AnimatedPadding(
+          duration: const Duration(milliseconds: 200),
+          padding: branchIndex == 0
+              ? EdgeInsets.only(right: 20.w, bottom: 20.h, left: 20.w)
+              : const EdgeInsets.all(0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.all(
+              branchIndex == 0
+                  ? Radius.circular(50.r)
+                  : const Radius.circular(0),
+            ),
+            child: BottomNavigationBar(
+              currentIndex: child.currentIndex,
+              selectedFontSize: 0,
+              unselectedFontSize: 0,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              backgroundColor: ColorClass.under,
+              selectedItemColor: ColorClass.blue,
+              unselectedItemColor: ColorClass.border,
+              onTap: bottomItemTapped,
+              items: const [
+                BottomNavigationBarItem(
+                  label: 'Home',
+                  icon: SizedBox(
+                    child: Icon(Icons.home_filled),
+                  ),
                 ),
-              ),
-              // BottomNavigationBarItem(
-              //   label: 'Calendar',
-              //   icon: Icon(Icons.event_available),
-              // ),
-              BottomNavigationBarItem(
-                label: 'Setting',
-                icon: Icon(Icons.settings),
-              ),
-            ],
+                BottomNavigationBarItem(
+                  label: 'Calendar',
+                  icon: Icon(Icons.event_available),
+                ),
+                BottomNavigationBarItem(
+                  label: 'Setting',
+                  icon: Icon(Icons.settings),
+                ),
+              ],
+            ),
           ),
         ),
       ),
